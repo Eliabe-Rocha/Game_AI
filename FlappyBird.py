057f21b64afef8008d3fdea82298b8c00003cfa1
Eliabe Rocha Da Cruz <62529397+Eliabe-Rocha@users.noreply.github.com>
2023-11-26 18:02:28 -0300

    First commit

---
+++ FlappyBird.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+import random
+pygame.
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+IMGEM_CANE = pygame.image.load(os.path.join('imgs', 'pipe.png'))
+
+IMAGEM_CHAO = 
+IMAGEM_BACKGROUND = 
+IMAGENS_PASSARO = 
+
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont("arial", 50)
